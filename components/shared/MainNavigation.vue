76e65ef7a7ce803ccd4912a249e6ab59c1b89d5d
AndiUchiha1 <andiuchihahaha@gmail.com>
2022-09-20 23:29:28 -0500

    chore: added navbar to layour
    Signed-off-by: AndiUchiha1 <andiuchihahaha@gmail.com>

---
+++ components/shared/MainNavigation.vue
@@ -0,0 +1,19 @@
+<template>
+  <!-- eslint-disable-next-line vue/no-v-model-argument -->
+  <a-menu v-model:selectedKeys="current" mode="horizontal">
+    <a-menu-item key="mail">
+      <template #icon />
+      Navigation One
+    </a-menu-item>
+    <a-menu-item key="app">
+      <template #icon />
+      Navigation Two
+    </a-menu-item>
+  </a-menu>
+</template>
+
+<script>
+export default {
+  name: 'MainNavigation'
+}
+</script>
